4319b8f98e2b14d1f380fdce019b1e6f775ec35e
mahedi5061 <mahedi5061@gmail.com>
2021-02-05 15:25:26 +0600

    update

---
+++ json.js
@@ -0,0 +1,6 @@
+// const user={id:61, name:'Sharif'}
+//         const userJson=JSON.stringify(user); //this line JS object property convert to string. 
+//         console.log(userJson);
+//         const userFromJson=JSON.parse(userJson); //this line receive the JSON property as as JS object property. 
+//         console.log(userFromJson);
+
